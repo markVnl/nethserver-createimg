3d36f45479553370bc7c86f2fe924d98514cbbf6
Mark Verlinde <mark@havak.nl>
2018-07-10 18:26:48 +0200

    Kickstart for Nethserver

--- build-armhfp-img.sh
+++ build-armhfp-img.sh
@@ -8,5 +8,5 @@ fi
 
 img=$(echo $ks|rev|cut -f 1 -d "/"|rev|sed s/\.ks//g)
 
-time appliance-creator --config=${ks} --name="CentOS-Userland-7-armv7hl-$img-1804" --version="7" --debug
+time appliance-creator --config=${ks} --name="$img" --version="7" --debug  --no-compress
 
